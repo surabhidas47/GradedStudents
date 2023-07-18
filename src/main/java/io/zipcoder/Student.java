bc1095e9736b6a2cb005ebb1a7af7e52831c6129
surabhi <surabhi.das@outlook.com>
2023-07-17 22:07:35 -0400

    starting project

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,16 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    String firstName;
+    String lastName;
+
+    ArrayList<Double> examScores;
+
+    public Student() {
+
+    }
 }
+
+
