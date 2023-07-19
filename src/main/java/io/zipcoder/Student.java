1f03bfe627429c8b47766f6bc03a081644a31450
surabhi <surabhi.das@outlook.com>
2023-07-18 21:14:32 -0400

    did some tests

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,16 +1,57 @@
 package io.zipcoder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Student {
-    String firstName;
-    String lastName;
+    private String firstName;
+    private String lastName;
 
-    ArrayList<Double> examScores;
+    private ArrayList<Double> examScores;
 
-    public Student() {
+    public Student(String firstName,String lastName, Double [] testScores ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(Arrays.asList(testScores));
+    }
+
+        public void setFirstName (String firstName){
+            this.firstName = firstName;
+        }
+
+        public String getFirstName (){
+            return this.firstName = firstName;
+        }
+
+        public void setLastName (String lastName){
+            this.lastName = lastName;
+        }
+
+        public String getLastName (){
+            return this.lastName = lastName;
+        }
+
+//    public String getExamScores() {
+//
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        sb.
+//
+//
+//
+//
+//    }
+
+
+//    public int getNumberOfExamsTaken() {
+//
+//            // returns the total number of exams taken by this student.
+//        }
+
+
 
     }
-}
+
 
 
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,82 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void testGetFirstName() {
+        //given
+        //if we put a name in the expected than that would be the
+        Double [] testScores = {97.0, 56.5,3.0};
+        Student s = new Student("Surabhi", "Das",testScores);
+        String expected = "Surabhi";
+
+
+        // When
+        String actual = s.getFirstName();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSetFirstName() {
+        //given
+        //if we put a name in the expected than that would be the
+        Double [] testScores = {97.0, 56.5,3.0};
+        Student s = new Student("Surabhi", "Das",testScores);
+        String expected = "Surabhi";
+
+        // When
+        String actual = s.getFirstName();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testGetLastName() {
+        //given
+        //if we put a name in the expected than that would be the
+        Double [] testScores = {97.0, 56.5,3.0};
+        Student s = new Student("Surabhi", "Das",testScores);
+        String expected = "Das";
+
+
+        // When
+        String actual = s.getLastName();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSetLastName() {
+        //given
+        //if we put a name in the expected than that would be the
+        Double [] testScores = {97.0, 56.5,3.0};
+        Student s = new Student("Surabhi", "Das",testScores);
+        String expected = "Das";
+
+
+        // When
+        String actual = s.getLastName();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testGetExamScore() {
+
+
+    }
+
 
 }
